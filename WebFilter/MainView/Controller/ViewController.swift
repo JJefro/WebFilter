af382a55b3bb1239088630d85ff1205a7d1f6fda
JJefro <jevjefro@gmail.com>
2022-01-22 17:47:19 +0200

    Initial commit

---
+++ WebFilter/MainView/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WebFilter
+//
+//  Created by Jevgenijs Jefrosinins on 22/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "WebFilter"
+        view.backgroundColor = .yellow
+    }
+}
